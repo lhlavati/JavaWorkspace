091a5c632c9fc504b5a1e9d618124660d7deb618
lhlavati <lukahlavati@gmail.com>
2019-06-23 18:37:04 +0200

    Vjezba Zadatak4

--- Edunova04/src/edunova/VjezbaNizova.java
+++ Edunova04/src/edunova/VjezbaNizova.java
@@ -50,33 +50,52 @@ public class VjezbaNizova {
 //		}
 //		asVecih = zbroj1 / brojac1;
 //		System.out.println("Aritmeticka sredina članova većih od aritmetičke sredine svih članova niza: " + asVecih);
+//		
+//		System.out.println("\nZadatak3\n");
+//		//Učitati članove niza. Izračunati aritmetičku sredinu i ispisati sumu članova niza manjih od aritmetičke sredine sa neparnim indeksom.
+//		int clanovi = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj članova niza:"));
+//		int niz[] = new int[clanovi];
+//		
+//		double as;
+//		double suma = 0;
+//		double zbroj = 0;
+//		double brojac = 0;
+//		for (int i = 0; i < niz.length; i++) {
+//			niz[i] = Integer.parseInt(JOptionPane.showInputDialog("Unesite članove:"));
+//		}
+//		
+//		for (int i = 0; i < niz.length; i++) {
+//			zbroj += niz[i];
+//			brojac++;
+//		}
+//		as = zbroj / brojac;
+//		System.out.println("Aritmetička sredina niza: " + as);
+//
+//		for (int i = 0; i < niz.length; i++) {
+//			if(niz[i] < as && i % 2 != 0) {
+//				suma += niz[i];
+//			}
+//		}
+//		System.out.println("Suma članova niza manjih od aritmetičke sredine s neparnim indeksima je: " + suma);
+//		
+		System.out.println("\nZadatak4\n");
+		// Učitati članove i naći najmanji član niza.
+		 
+		int clanovi = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj clanova niza:"));
 		
-		System.out.println("\nZadatak3\n");
-		//Učitati članove niza. Izračunati aritmetičku sredinu i ispisati sumu članova niza manjih od aritmetičke sredine sa neparnim indeksom.
-		int clanovi = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj članova niza:"));
-		int niz[] = new int[clanovi];
+		int[] niz = new int [clanovi];
 		
-		double as;
-		double suma = 0;
-		double zbroj = 0;
-		double brojac = 0;
 		for (int i = 0; i < niz.length; i++) {
-			niz[i] = Integer.parseInt(JOptionPane.showInputDialog("Unesite članove:"));
+			niz[i] = Integer.parseInt(JOptionPane.showInputDialog("Unesite clanove niza"));
 		}
-		
+		int min = niz[0];
 		for (int i = 0; i < niz.length; i++) {
-			zbroj += niz[i];
-			brojac++;
-		}
-		as = zbroj / brojac;
-		System.out.println("Aritmetička sredina niza: " + as);
-
-		for (int i = 0; i < niz.length; i++) {
-			if(niz[i] < as && i % 2 != 0) {
-				suma += niz[i];
+			if(min > niz[i]) {
+				min = niz[i];
 			}
 		}
-		System.out.println("Suma članova niza manjih od aritmetičke sredine s neparnim indeksima je: " + suma);
+		System.out.println("Najmanji član niza je: " + min);
 		
+		System.out.println("\nZadatak5\n");
 	}
 }
