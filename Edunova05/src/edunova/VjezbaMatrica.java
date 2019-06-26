e07203bd783344bd1402057aefe3cd8054c18a62
lhlavati <lukahlavati@gmail.com>
2019-06-27 01:58:35 +0200

    Vjezba Zadatak2

--- Edunova05/src/edunova/VjezbaMatrica.java
+++ Edunova05/src/edunova/VjezbaMatrica.java
@@ -4,20 +4,38 @@ public class VjezbaMatrica {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Zadatak1\n");
-//		Napisati program da ispisuje 3 puta u 3 reda IME sa razmakom:
-//				    IME IME IME
-//				    IME IME IME
-//				    IME IME IME
+//		System.out.println("Zadatak1\n");
+//Napisati program da ispisuje 3 puta u 3 reda IME sa razmakom:
+//		    IME IME IME
+//		    IME IME IME
+//		    IME IME IME
 //		
+//		int [][] ime = new int [3][3];
+//		
+//		for (int i = 0; i < ime.length; i++) {
+//			for (int j = 0; j < ime.length; j++) {
+//				System.out.print("IME ");
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		
+		System.out.println("\nZadatak2\n");
+		// Napisati program da ispisuje u prvom redu jedno ime a zatim u svakom narednom po jedno više
+		//		IME
+		//		IME IME
+		//		IME IME IME
 		int [][] ime = new int [3][3];
 		
 		for (int i = 0; i < ime.length; i++) {
 			for (int j = 0; j < ime.length; j++) {
-				System.out.print("IME ");
+				if(i >= j) {
+					System.out.print("IME ");	
+				}
 			}
 			System.out.println();
 		}
 		System.out.println();
+		
 	}
 }
