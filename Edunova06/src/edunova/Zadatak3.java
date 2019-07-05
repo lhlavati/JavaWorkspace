63481ce4ad003198120b37c9ca4f5689da11fce8
lhlavati <lukahlavati@gmail.com>
2019-07-05 16:29:20 +0200

    Vjezba, Zadatak3

--- Edunova06/src/edunova/Zadatak3.java
+++ Edunova06/src/edunova/Zadatak3.java
@@ -1,8 +1,29 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak3 {
 	
 	// napišite metodu koja prima dvodimenzinalni niz (matrica)
-	// metoda vraæa nazad sumu svih elemenata not niza
+	// metoda vraæa nazad sumu svih elemenata tog niza
 
+	public static void main(String[] args) {
+		int[][] niz = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		System.out.println(suma(niz));
+	}
+	
+	private static int suma(int [][] niz) {
+		int suma = 0;
+		for(int i = 0; i < niz.length; i++) {
+			for(int j = 0; j < niz.length; j++) {
+				suma += niz[i][j];
+			}
+		}
+		
+		return suma;
+	}
 }
