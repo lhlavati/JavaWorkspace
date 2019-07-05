72069668015232f6d9497a6b8a034284cb16c6c5
lhlavati <lukahlavati@gmail.com>
2019-07-05 19:26:52 +0200

    Vjezba1, Vjezba2

---
+++ Edunova06/src/edunova/Vjezba1.java
@@ -0,0 +1,11 @@
+package edunova;
+
+public class Vjezba1 {
+	// 1. Main poziva metodu Ispis i prenosi izabrano ime. Metoda ispisuje Zdravo i izabrano ime.
+	public static void main(String[] args) {
+		ispis("Luka");
+	}
+	public static void ispis(String ime) {
+		System.out.println("Zdravo " + ime);
+	}
+}
---
+++ Edunova06/src/edunova/Vjezba2.java
@@ -0,0 +1,11 @@
+package edunova;
+
+public class Vjezba2 {
+	// Main poziva metodu suma, metoda suma ispisuje sumu tri broja
+	public static void main(String[] args) {
+		suma(2,3,4);
+	}
+	public static void suma(int a, int b, int c) {
+		System.out.println("Suma ova tri broja je: " + (a+b+c));
+	}
+}
