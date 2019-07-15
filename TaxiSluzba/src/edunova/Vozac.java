bc8c5b4594c52de8d28cf153f75ce66928a5c167
lhlavati <lukahlavati@gmail.com>
2019-07-15 13:50:38 +0200

    Update vozac

--- TaxiSluzba/src/edunova/Vozac.java
+++ TaxiSluzba/src/edunova/Vozac.java
@@ -38,4 +38,35 @@ public class Vozac {
 	public void setSpol(String spol) {
 		this.spol = spol;
 	}
+	
+	@Override
+	public String toString() {
+		return this.ime + " " + this.prezime;
+	}
+	
+	public static boolean provjeraOib(String oib) {
+		if (oib.length() != 11)
+			return false;
+
+		try {
+			Long.parseLong(oib);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		int a = 10;
+		for (int i = 0; i < 10; i++) {
+			a = a + Integer.parseInt(oib.substring(i, i + 1));
+			a = a % 10;
+			if (a == 0)
+				a = 10;
+			a *= 2;
+			a = a % 11;
+		}
+		int kontrolni = 11 - a;
+		if (kontrolni == 10)
+			kontrolni = 0;
+
+		return kontrolni == Integer.parseInt(oib.substring(10));
+	}
 }
