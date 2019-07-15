9863bbfd0a1309f7d53010c11fb012fbc62b4530
lhlavati <lukahlavati@gmail.com>
2019-07-15 13:50:48 +0200

    Dodan vozi

---
+++ TaxiSluzba/src/edunova/Vozi.java
@@ -0,0 +1,22 @@
+package edunova;
+
+import java.util.Date;
+
+public class Vozi {
+
+	private Date vrijemePocetka;
+	private Date vrijemeKraja;
+	
+	public Date getVrijemePocetka() {
+		return vrijemePocetka;
+	}
+	public void setVrijemePocetka(Date vrijemePocetka) {
+		this.vrijemePocetka = vrijemePocetka;
+	}
+	public Date getVrijemeKraja() {
+		return vrijemeKraja;
+	}
+	public void setVrijemeKraja(Date vrijemeKraja) {
+		this.vrijemeKraja = vrijemeKraja;
+	}
+}
