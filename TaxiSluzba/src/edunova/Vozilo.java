4926232ea982fe1662d10876990e77f784944d47
lhlavati <lukahlavati@gmail.com>
2019-07-15 13:30:51 +0200

    Dodano vozilo

---
+++ TaxiSluzba/src/edunova/Vozilo.java
@@ -0,0 +1,48 @@
+package edunova;
+
+public class Vozilo {
+
+	private int sifra;
+	private String marka;
+	private String snaga;
+	private int abs;
+	private String godiste;
+	private int brojvozila;
+	
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getMarka() {
+		return marka;
+	}
+	public void setMarka(String marka) {
+		this.marka = marka;
+	}
+	public String getSnaga() {
+		return snaga;
+	}
+	public void setSnaga(String snaga) {
+		this.snaga = snaga;
+	}
+	public int getAbs() {
+		return abs;
+	}
+	public void setAbs(int abs) {
+		this.abs = abs;
+	}
+	public String getGodiste() {
+		return godiste;
+	}
+	public void setGodiste(String godiste) {
+		this.godiste = godiste;
+	}
+	public int getBrojvozila() {
+		return brojvozila;
+	}
+	public void setBrojvozila(int brojvozila) {
+		this.brojvozila = brojvozila;
+	}
+}
