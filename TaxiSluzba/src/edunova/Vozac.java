b3ac13a666870130f4b3d8f7777af6a57295a5f4
lhlavati <lukahlavati@gmail.com>
2019-07-15 13:32:33 +0200

    Dodan vozac

---
+++ TaxiSluzba/src/edunova/Vozac.java
@@ -0,0 +1,41 @@
+package edunova;
+
+public class Vozac {
+
+	private int sifra;
+	private String ime;
+	private String prezime;
+	private String oib;
+	private String spol;
+	
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getOib() {
+		return oib;
+	}
+	public void setOib(String oib) {
+		this.oib = oib;
+	}
+	public String getSpol() {
+		return spol;
+	}
+	public void setSpol(String spol) {
+		this.spol = spol;
+	}
+}
