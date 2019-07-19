3f76102966e8e7ec2676d5ca368eac3e51532240
lhlavati <50337055+lhlavati@users.noreply.github.com>
2019-07-19 18:58:45 +0200

    Logiranje korisnika u aplikaciju

--- TaxiSluzba/src/edunova/Start.java
+++ TaxiSluzba/src/edunova/Start.java
@@ -1,12 +1,43 @@
 package edunova;
 
-public class Start {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.swing.JOptionPane;
+
+public class Start {
+	
+	public static final String FORMAT_DATUMA = "dd.MM.yyyy. HH:mm";
+	
 	public Start() {
 		
-		
+		logIn();
 		
 	}
+	
+	private void logIn() {
+		
+		Vozac vozac = new Vozac();
+		Vozi vozi = new Vozi();
+		
+		vozac.setIme(JOptionPane.showInputDialog("Unesite ime:"));
+		vozac.setPrezime(JOptionPane.showInputDialog("Unesite prezime"));
+		while(true) {
+			try {
+				vozac.setSifra(Integer.parseInt(JOptionPane.showInputDialog("Unesite šifru")));
+				JOptionPane.showMessageDialog(null, "Pristup odobren! Dobar dan " + vozac.getIme().trim() + " " + vozac.getPrezime().trim() + "!");
+				break;
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Pristup odbijen!");
+				continue;
+			}
+		}
+		SimpleDateFormat df = new SimpleDateFormat(FORMAT_DATUMA);
+		System.out.println("Vrijeme pocetka smjene");
+		vozi.setVrijemePocetka(new Date(FORMAT_DATUMA));
+		System.out.println(vozi.getVrijemePocetka());
+	}
+	
 	public static void main(String[] args) {
 		new Start();
 	}
