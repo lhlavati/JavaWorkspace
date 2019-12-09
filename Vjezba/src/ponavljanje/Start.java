2896df792ed16dc1c6acf422dcfc4c13fa07d305
lhlavati <lukahlavati@gmail.com>
2019-12-09 23:40:13 +0100

    Tri zadatka za ponavljanje

---
+++ Vjezba/src/ponavljanje/Start.java
@@ -0,0 +1,37 @@
+package ponavljanje;
+
+import java.util.Scanner;
+
+public class Start {
+	public static void main(String[] args) {
+//		#ZADATAK1
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("Input first number");
+//		int num1 = input.nextInt();
+//		System.out.println("Input second number");
+//		int num2 = input.nextInt();
+//		System.out.println("Input third number");
+//		int num3 = input.nextInt();
+//		int average = (num1 + num2 + num3) / 3;
+//		System.out.println("Average of " + num1 + ", " + num2 + " and " + num3 + " is: " + average);
+		
+//		#ZADATAK2
+//		Scanner scanner = new Scanner(System.in);
+//        System.out.print("Input a string: ");
+//        char[] letters = scanner.nextLine().toCharArray();
+//        System.out.print("Reverse string: ");
+//        for (int i = letters.length - 1; i >= 0; i--) {
+//            System.out.print(letters[i]);
+//        }
+//        System.out.print("\n");
+		
+//		#ZADATAK3
+//		System.out.println("Input a string: ");
+//		Scanner input = new Scanner(System.in);
+//		String number = input.nextLine();
+//		System.out.println("Integer value of " + number + " is: " + Integer.parseInt(number));
+		
+//		#ZADATAK4
+		
+	}
+}
